b337b726f084223d0b3f504559bc8a0eaa2d4926
Gabriel Spioni <86626937+GAGabis@users.noreply.github.com>
2023-06-13 18:41:43 -0300

    v1

---
+++ testElectron/changeToScreen2.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+
+button1.addEventListener('click', () => {
+  
+  window.location.href = 'src/screen2.html';
+});
+
+button2.addEventListener('click', () => {
+  window.location.href = 'src/screen2.html';
+});
+
+button3.addEventListener('click', () => {
+  window.location.href = 'src/screen2.html';
+});
---
+++ testElectron/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('src/index.html');
+
+ 
+
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
