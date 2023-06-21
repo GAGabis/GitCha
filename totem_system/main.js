4796e67deca4c7802156defe217ccbdbc8b23b7a
Gabriel Spioni <86626937+GAGabis@users.noreply.github.com>
2023-06-20 21:24:43 -0300

    v8.1

--- totem_system/main.js
+++ totem_system/main.js
@@ -4,8 +4,8 @@ const { PythonShell } = require('python-shell');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       nodeIntegration: true
     }
@@ -17,28 +17,22 @@ function createWindow() {
     app.quit();
   });
 
-  // Executa o script Python quando a janela for criada
   runPythonScript(mainWindow);
 }
 
 function runPythonScript(mainWindow) {
   let options = {
     scriptPath: path.join(__dirname, 'python_scripts'),
-    pythonOptions: ['-u'], // Opção para exibir a saída do Python em tempo real
+    pythonOptions: ['-u'], 
   };
 
-  // Cria a instância do PythonShell para executar o script Python
   const pyshell = new PythonShell('reconhecimento_faces.py', options);
 
-  // Lida com a saída do Python
   pyshell.on('message', (message) => {
-    // Envia a saída do Python para a janela principal do Electron
     mainWindow.webContents.send('python-output', message);
   });
 
-  // Lida com o encerramento do PythonShell
   pyshell.on('close', () => {
-    // Encerra a aplicação do Electron quando o script Python terminar
     app.quit();
   });
 }
@@ -59,15 +53,10 @@ app.on('window-all-closed', () => {
   }
 });
 
-// Lida com a recepção da saída do Python na janela principal do Electron
 ipcMain.on('python-output', (event, message) => {
   console.log('Saída do Python:', message);
-
-  // Verifica se a mensagem é "Face detected!"
   if (message === 'Face detected!') {
-    // Faça algo quando uma face for detectada
     console.log('Face detectada!');
-    // Exemplo: exibir uma notificação no Electron
     const { Notification } = require('electron');
     const notification = new Notification({
       title: 'Detecção de Face',
