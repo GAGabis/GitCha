9c6f4ee27df0b1fbbec718beca1da69ca7361d97
Gabriel Spioni <86626937+GAGabis@users.noreply.github.com>
2023-06-27 21:44:49 -0300

    v10.5

--- totem_system/js_scripts/clientCadastr.js
+++ totem_system/js_scripts/clientCadastr.js
@@ -47,46 +47,8 @@ function SubmitEvent(event) {
   console.log(dataNascimento);
   console.log(numConvenio);
   if (nome == 'gabriel') {
-    window.location.href = 'index.html';
+    window.location.href = 'starting.html';
 
   }
 
-}
-
-function toggleClickedState(event) {
-  event.preventDefault();
-  var whatsappButton = document.querySelector('.whatsapp-button');
-  whatsappButton.classList.toggle('clicked');
-}
-
-function handleButtonClick(option) {
-  if (option === "Sim") {
-    console.log("Você clicou em 'Sim'");
-
-    if ('speechSynthesis' in window) {
-      var utterance = new SpeechSynthesisUtterance("O assistente foi ativado.");
-
-      // Inicia a síntese de voz
-      window.speechSynthesis.speak(utterance);
-      eventSpeak();
-    } else {
-      console.log("A síntese de voz não é suportada neste navegador.");
-    }
-  } else if (option === "Não") {
-    console.log("Você clicou em 'Não'");
-  }
-}
-
-
-function eventSpeak() {
-  const listInputs = document.querySelectorAll('input');
-  
-  listInputs.forEach(input => {
-    const functionTTS = () => {
-      const phraseSpeak = input.getAttribute('name');
-      let phraseInstance= new SpeechSynthesisUtterance(phraseSpeak);
-      window.speechSynthesis.speak(phraseInstance); 
-    }
-    input.addEventListener("click", functionTTS);
-  })
 }
