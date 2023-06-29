bd6bcd9a4525466f3b166adfc5d524c3c166d7b6
Gabriel Spioni <86626937+GAGabis@users.noreply.github.com>
2023-06-28 21:35:20 -0300

    v12

--- totem_system/js_scripts/screenTotemForms_ClientRegister_and_PaySystem_.js
+++ totem_system/js_scripts/screenTotemForms_ClientRegister_and_PaySystem_.js
@@ -1,4 +1,3 @@
-const cpfInput = document.getElementById('cpf');
 let currentURL = localStorage.getItem('currentUrl');
 
 if (currentURL) {
@@ -6,11 +5,13 @@ if (currentURL) {
   let searchParams = new URLSearchParams(url.search);
 
   if (searchParams.has('btn')) {
-    let btnValue = searchParams.get('btn');
+    var btnValue = searchParams.get('btn');
     console.log(btnValue);
   }
 }
 
+
+const cpfInput = document.getElementById('cpf');
 cpfInput.addEventListener('input', function () {
   let cpf = cpfInput.value;
 
@@ -41,28 +42,52 @@ dataNascimentoInput.addEventListener('input', function () {
   dataNascimentoInput.value = dataNascimento;
 });
 
-
-
-
 function SubmitEvent(event) {
   event.preventDefault();
-
-
+  
   var nome = document.getElementById('nome').value;
   var cpf = document.getElementById('cpf').value;
   var dataNascimento = document.getElementById('data_nascimento').value;
   var numConvenio = document.getElementById('num_convenio').value;
+
+
+  // SIMULAÇÃO TABELA DE PREÇO:
+  //CASE 1: PLANOS (ST/MD/PL) - VALORES A DEFINIR
+
+  let stCoberturaClinico = 60; 
+  let stCoberturaGeneric = 0;
+  let stCoberturaAgendado = 0;
+
+  let mdCoberturaClinico = 85;
+  let mdCoberturaGeneric = 0;
+  let mdCoberturaAgendado = 0;
+
+  let plCoberturaClinico = 115;
+  let plCoberturaGeneric = 0;
+  let plCobertuarAgendado = 0;
+  
+  let clinicoGeralCase1 = 100;
+
   if (nome !== '' && cpf.length === 14 && dataNascimento.length === 10 && numConvenio !== '') {
-    window.location.href = 'screenCompleteTotem.html';
+    switch (btnValue) {
+      case 'clinico':
+        let convenioPlano = numConvenio.slice(0,2);
+        if(convenioPlano === 'ST'){
+          let valorSubstancial = clinicoGeralCase1 - stCoberturaClinico;
+          console.log(valorSubstancial);
+          //gerar boleto para pagar a diferença - conexão com o APP ou NÚMERO
+        }
+
+      case '':
+        break
+
+    }
   } else {
-    
+
     alert('Preencha todos os campos corretamente.');
   }
   console.log(nome);
   console.log(cpf);
   console.log(dataNascimento);
   console.log(numConvenio);
-  
-
-
 }
